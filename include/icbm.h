7a69cfb83e76c34acd914d7dbea006963c27c311
dutch <chris@lamberson.online>
2020-11-02 12:14:12 -0800

    refactor fingerprinting

--- include/icbm.h
+++ include/icbm.h
@@ -1,8 +1,6 @@
 #ifndef ICBM_HEADER
 #define ICBM_HEADER
 
-const char *getverstr(void);
-void parse(const char *);
 int icbm_get(const char *);
 
 #endif
--- lib/icbm.c
+++ lib/icbm.c
@@ -6,47 +6,40 @@
 #include <openssl/evp.h>
 #include <openssl/x509.h>
 
-extern int yyparse(void);
-
-const char *
-getverstr(void)
-{
-  return PACKAGE_STRING;
-}
-
-void
-parse(const char *src)
-{
-  yyparse();
-}
-
 void
 yyerror(const char *s)
 {
   fprintf(stderr, "%s\n", s);
 }
 
+static int
+BIO_write_fingerprint(BIO *b, X509 *cert)
+{
+  int i;
+  unsigned int buflen;
+  unsigned char buf[EVP_MAX_MD_SIZE];
+
+  if (X509_digest(cert, EVP_sha1(), buf, &buflen) == 0)
+    return 1;
+
+  for (i = 0; i < buflen; ++i)
+    BIO_printf(b, "%02X%c", buf[i], (i + 1 == buflen) ? '\n' : ':');
+
+  return 0;
+}
+
 int
 icbm_get(const char *url)
 {
-  const SSL_METHOD *method;
-  const EVP_MD *digest;
   SSL_CTX *ctx;
-  BIO *space;
+  BIO *remote, *err;
   SSL *ssl;
   X509 *cert;
-  unsigned char buf[EVP_MAX_MD_SIZE];
-  unsigned int buflen;
 
   SSL_library_init();
   SSL_load_error_strings();
 
-  if (!(method = TLS_method())) {
-    fputs("TLS_method\n", stderr);
-    return EXIT_FAILURE;
-  }
-
-  if (!(ctx = SSL_CTX_new(method))) {
+  if (!(ctx = SSL_CTX_new(TLS_method()))) {
     fputs("SSL_CTX_new\n", stderr);
     return EXIT_FAILURE;
   }
@@ -56,28 +49,28 @@ icbm_get(const char *url)
     return EXIT_FAILURE;
   }
 
-  if (!(space = BIO_new_ssl_connect(ctx))) {
+  if (!(remote = BIO_new_ssl_connect(ctx))) {
     fputs("BIO_new_ssl_connect\n", stderr);
     return EXIT_FAILURE;
   }
 
-  if (BIO_set_conn_hostname(space, url) == 0) {
+  if (BIO_set_conn_hostname(remote, url) == 0) {
     fputs("BIO_set_conn_hostname\n", stderr);
     return EXIT_FAILURE;
   }
 
-  BIO_get_ssl(space, &ssl);
+  BIO_get_ssl(remote, &ssl);
   if (!ssl) {
     fputs("BIO_get_ssl\n", stderr);
     return EXIT_FAILURE;
   }
 
-  if (BIO_do_connect(space) == 0) {
+  if (BIO_do_connect(remote) == 0) {
     fputs("BIO_do_connect\n", stderr);
     return EXIT_FAILURE;
   }
 
-  if (BIO_do_handshake(space) == 0) {
+  if (BIO_do_handshake(remote) == 0) {
     fputs("BIO_do_handshake\n", stderr);
     return EXIT_FAILURE;
   }
@@ -87,12 +80,12 @@ icbm_get(const char *url)
     return EXIT_FAILURE;
   }
 
-  X509_digest(cert, EVP_sha1(), buf, &buflen);
+  err = BIO_new_fp(stderr, BIO_NOCLOSE);
+  BIO_write_fingerprint(err, cert);
+
   X509_free(cert);
-
-  fprintf(stderr, "fingerprint: %.*s\n", buflen, buf);
-
-  BIO_free_all(space);
+  BIO_free_all(remote);
+  BIO_free_all(err);
   SSL_CTX_free(ctx);
 
   return EXIT_SUCCESS;
